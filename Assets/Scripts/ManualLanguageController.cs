dd9895fca9abdf485cf497883cd7e1cab2ae52e0
legendarycoconutkai <73057952+legendarycoconutkai@users.noreply.github.com>
2024-04-18 22:38:26 +0800

    Updated Book language

--- Assets/Scripts/ManualLanguageController.cs
+++ Assets/Scripts/ManualLanguageController.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ManualLanguageController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] manual;
+    public TextMeshProUGUI text;
+
+    private bool isEng = true;
+
+    public void ChangeLangauge()
     {
-        
+        if (!isEng)
+        {
+            isEng = true;
+            text.text = "EN";
+        }
+        else
+        {
+            isEng = false;
+            text.text = "ID";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenManual()
     {
-        
+        if (isEng)
+        {
+            manual[0].SetActive(true);
+        }
+        else
+        {
+            manual[1].SetActive(true);
+        }
     }
 }
