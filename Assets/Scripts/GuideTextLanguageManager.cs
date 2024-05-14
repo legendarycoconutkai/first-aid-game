99e2e273596bf39bff34af2601366f53d4693615
legendarycoconutkai <73057952+legendarycoconutkai@users.noreply.github.com>
2024-05-15 01:04:59 +0800

    Update Popout Guide Text

--- Assets/Scripts/GuideTextLanguageManager.cs
+++ Assets/Scripts/GuideTextLanguageManager.cs
@@ -8,6 +8,7 @@ public class GuideTextLanguageManager : MonoBehaviour
     string[] englishText = new string[] { "Danger", "Response", "Send for help", "Airway", "Breathing", "Circulation" };
     string[] indoText = new string[] { "Danger\n(Bahaya)", "Response\n(Respons)", "Send for help\n(Panggilan Untuk Bantuan)", "Airway\n(Saluran Udara)", "Breathing\n(Pernafasan)", "Circulation\n(Sirkulasi)" };
     public GameObject textObject;
+    public GameObject textBg;
     public TextMeshProUGUI text;
     public bool[] PrimarySurvey = new bool[6];
 
@@ -24,11 +25,13 @@ public class GuideTextLanguageManager : MonoBehaviour
         if (isEng)
         {
             textObject.SetActive(true);
+            textBg.SetActive(true);
             text.text = englishText[i];
         }
         else
         {
             textObject.SetActive(true);
+            textBg.SetActive(true);
             text.text = indoText[i];
         }
     }
