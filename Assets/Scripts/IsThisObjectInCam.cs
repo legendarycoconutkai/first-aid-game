f461f4bfd5f311f169e56856e78917526f9c067e
legendarycoconutkai <73057952+legendarycoconutkai@users.noreply.github.com>
2024-05-17 03:17:13 +0800

    Update IsThisObjectInCam.cs

--- Assets/Scripts/IsThisObjectInCam.cs
+++ Assets/Scripts/IsThisObjectInCam.cs
@@ -16,7 +16,7 @@ public class IsThisObjectInCam : MonoBehaviour
     void Update()
     {
         Vector3 viewPos = cam.WorldToViewportPoint(target.position);
-        if (viewPos.x > 0 && viewPos.y>0)
+        if (viewPos.x > 0 && viewPos.x < 1 && viewPos.y > 0 && viewPos.y < 1)
             Debug.Log("target is in the camera view!");
         else
             Debug.Log("target is not in the camera view!");
