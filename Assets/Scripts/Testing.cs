0fe8eb2e6cdaa03b92a61c2f42810564b7c537e6
junhubdotcom <furnoyong@gmail.com>
2024-05-17 02:47:42 +0800

    update

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
-    public TextMeshProUGUI text;
-    public void Clicked(string t)
+    
+
+    Renderer m_Renderer;
+    // Use this for initialization
+    void Start()
     {
-        text.text += t;
+        m_Renderer = GetComponent<Renderer>();
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_Renderer.isVisible)
+        {
+            Debug.Log("Object is visible");
+        }
+        else Debug.Log("Object is no longer visible");
+    }
+
 }
