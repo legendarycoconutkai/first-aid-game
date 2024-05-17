c84a05cf8aa8e993040b7fb9962270658436b32f
junhubdotcom <furnoyong@gmail.com>
2024-05-18 03:50:09 +0800

    wound generation

--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -8,19 +8,26 @@ public class SpriteChanger : MonoBehaviour
     public SpriteRenderer spriteRenderer;
     public Sprite[] spriteArray;
     public int currentSprite;
+    public string injuryName;
+    public bool AdminSetting;
     bool isSpriteChanger = false;
     bool FirstTime = false;
+    private Dictionary<string, bool> injuries = new Dictionary<string, bool>();
     // Start is called before the first frame update
     void Start()
     {
+        GameObject ir = GameObject.Find("InfoRecorder");
+        injuries = ir.GetComponent<InfoToBeBroughtOver>().getInjuries();
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         currentSprite = 0;
+        AdminSetting = injuries[injuryName];
     }
 
     // Update is called once per frame
     void Update()
     {
         ChangeSprite();
+          
     }
 
     public void Change()
@@ -35,7 +42,16 @@ public class SpriteChanger : MonoBehaviour
         {
             spriteRenderer.sprite = spriteArray[currentSprite];
             currentSprite++;
-            bool isSpriteChange = false;
+            isSpriteChanger = false;
+            Debug.Log("is Sprite Change to false");
+        }
+
+        else if(AdminSetting && currentSprite < spriteArray.Length )
+        {
+            currentSprite++;
+            spriteRenderer.sprite = spriteArray[currentSprite];
+            AdminSetting = false;
+            isSpriteChanger = false;
             Debug.Log("is Sprite Change to false");
         }
 
