0b63fe3f2c2c857d9bdcac00ab480cb9fe34ba4b
legendarycoconutkai <73057952+legendarycoconutkai@users.noreply.github.com>
2024-05-18 17:59:41 +0800

    Fullscreen fix

--- Assets/Scripts/FullscreenScript.cs
+++ Assets/Scripts/FullscreenScript.cs
@@ -1,10 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FullscreenScript : MonoBehaviour
 {
     public GameObject fullScreenWarning;
+
+    public Button fullscreenButton;
+
+    void Start()
+    {
+        if (fullscreenButton != null)
+        {
+            fullscreenButton.onClick.AddListener(EnterFullscreen);
+        }
+        else
+        {
+            Debug.LogError("Fullscreen button is not assigned in the inspector.");
+        }
+    }
+
+    void EnterFullscreen()
+    {
+        Screen.fullScreen = true;
+        Debug.Log("Entering fullscreen mode.");
+    }
 
     // Action to trigger when the player is not in fullscreen mode
     private void TriggerAction()
